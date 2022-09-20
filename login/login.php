9b4fe3294b52693635dd71976ed9dee63ef2e67a
antonellaperlinghieri <51961677+antonellaperlinghieri@users.noreply.github.com>
2022-09-20 12:12:26 -0300

    se añade el login

---
+++ login/login.php
@@ -0,0 +1,16 @@
+<?php
+
+$usuario = $_POST ["user"];
+$contrasenia = $_POST ["pass"];
+
+$ckuser = "admin";
+$ckpass = "1485";
+
+if ($usuario == $ckuser && $contrasenia == $ckpass) {
+    echo "Correcto";
+} else {
+    echo "Incorrecto";
+}
+
+
+?>
