83a5e80ba2bd24bbfc7fc8175ad1e4a93353ae73
antonellaperlinghieri <51961677+antonellaperlinghieri@users.noreply.github.com>
2022-09-20 15:10:51 -0300

    se  añadio un parentesis al if

--- login/login.php
+++ login/login.php
@@ -1,12 +1,12 @@
 <?php
 
-$usuario = $_POST ["user"];
-$contrasenia = $_POST ["pass"];
+$usuario =$_POST["user"];
+$contrasenia =$_POST["pass"];
 
 $ckuser = "admin";
 $ckpass = "1485";
 
-if ($usuario == $ckuser && $contrasenia == $ckpass) {
+if (($usuario == $ckuser) && ($contrasenia == $ckpass)) {
     echo "Correcto";
 } else {
     echo "Incorrecto";
