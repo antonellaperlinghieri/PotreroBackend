ff0287277de36a179e1cc57d0ac321ffabe4e3e4
antonellaperlinghieri <51961677+antonellaperlinghieri@users.noreply.github.com>
2022-10-04 17:50:48 -0300

    Add files via upload

---
+++ potrero-back/TP4/tp4_backend.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es" dir = "ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP N 4</title>
+</head>
+<body>
+    <h3>Ejercicio N 1</h3>
+    <?php
+    $numerospares = [2,4,6,8,10,12,14,16,18,20];
+    echo "<br>";
+    print_r ($numerospares);
+    ?>
+    <h3>Ejercicio N 2</h3>
+    <?php
+    $datos = [ 'Pedro' => 34, 'Ana'  => 1];
+    print_r($datos);
+    ?>
+    <h3>Ejercicio N 3</h3>
+    <?php
+    $datos2 = ['nombre' => "Pedro" , 'apellido' => "Torres", 'direccion' => "Av Mayor 3703", 'telefono' => 1122334455];
+    print_r($datos2);
+    ?>
+    <h3>Ejercicio N 4</h3>
+    <?php
+    $ciudades = ["Madrid", "Barcelona", "Londres", "New York", "Los Angeles", "Chicago"];
+    print_r($ciudades);
+    foreach ($ciudades as $valor) {
+        print " <p> $valor </p>";
+    }
+
+    ?>
+</body>
+</html>
